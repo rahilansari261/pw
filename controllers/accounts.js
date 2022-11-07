42ca633c776c9cb546e450cb2b767a6d2934a3de
rahilansari261 <rahilansari261@gmail.com>
2022-11-07 11:43:53 +0530

    accounts api added

--- controllers/accounts.js
+++ controllers/accounts.js
@@ -65,8 +65,142 @@ const createAccount = async (req, res) => {
     res.status(200).json({message: error,success: false,})
   }
 }
+const getFindAndSortOptionsAccoToParams = async (
+  client_id,
+  searchString,
+  start_date,
+  end_date
+) => {
+  let findAndSortOptions = {}
+  let startDate
+  let endDate
+  if (start_date != 'no-start-date' && end_date != 'no-end-date') {
+    startDate = new Date(start_date)
+    endDate = new Date(end_date)
+    startDate.setHours(0, 0, 0, 0)
+    endDate.setHours(23, 59, 59, 999)
+  }
+  // 1-> getting accounts with date filter and paging
+  if (
+    client_id === 'no-id' &&
+    searchString === 'no-search' &&
+    start_date != 'no-start-date' &&
+    end_date != 'no-end-date'
+  ) {
+    findAndSortOptions.findOptions = {
+      entry_date: {
+        $gte: startDate,
+        $lt: endDate,
+      },
+    }
+    findAndSortOptions.sortOptions = {
+      _id: -1,
+    }
+  }
+  // 2-> getting accounts of a client(client_id) and with search string and paging
+  else if (
+    start_date === 'no-start-date' &&
+    end_date === 'no-end-date' &&
+    client_id != 'no-id' &&
+    searchString != 'no-search'
+  ) {
+    findAndSortOptions.findOptions = {
+      $and: [
+        { entry_remarks: new RegExp(req.params.searchString, 'i') },
+        { client_id: id },
+      ],
+    }
+    findAndSortOptions.sortOptions = { entry_date: -1 }
+  }
+  // 3-> getting accounts of a client(client_id) and paging
+  else if (
+    start_date === 'no-start-date' &&
+    end_date === 'no-end-date' &&
+    client_id != 'no-id'
+  ) {
+    findAndSortOptions.findOptions = { client_id: id }
+    findAndSortOptions.sortOptions = { entry_date: -1 }
+  }
+  // 4-> getting accounts of a client(client_id) with date filter and paging  (TYPICAL)
+  // else if (
+  //   start_date != 'no-start-date' &&
+  //   end_date != 'no-end-date' &&
+  //   client_id != 'no-id' &&
+  //   searchString === 'no-search'
+  // ) {
+  //   findAndSortOptions.findOptions = {
+  //     $and: [
+  //       { entry_date: { $gte: startDate, $lt: endDate } },
+  //       { client_id: id },
+  //     ],
+  //   }
+  //   findAndSortOptions.sortOptions = { entry_date: -1 }
+  // }
 
-const getAccountDetails = async (req, res) => {}
+  // 5-> getting accounts with search filter and paging
+  else if (
+    start_date === 'no-start-date' &&
+    end_date === 'no-end-date' &&
+    client_id === 'no-id' &&
+    searchString != 'no-search'
+  ) {
+    findAndSortOptions.findOptions = {
+      $and: [
+        {
+          $or: [
+            {
+              account_name: new RegExp(req.params.searchStr, 'i'),
+            },
+            {
+              account_company: new RegExp(req.params.searchStr, 'i'),
+            },
+          ],
+        },
+      ],
+    }
+    findAndSortOptions.sortOptions = { entry_date: -1 }
+  }
+
+  // 6-> getting accounts of a client(client_id) with search and date filter and paging
+  else if (
+    start_date != 'no-start-date' &&
+    end_date != 'no-end-date' &&
+    client_id != 'no-id' &&
+    searchString != 'no-search'
+  ) {
+    findAndSortOptions.findOptions = {
+      $and: [
+        {
+          $or: [{ entry_remarks: new RegExp(req.params.searchString, 'i') }],
+        },
+        { entry_date: { $gte: startDate, $lte: endDate } },
+        { client_id: id },
+      ],
+    }
+    findAndSortOptions.sortOptions = { entry_date: -1 }
+  }
+  return findAndSortOptions
+}
+const getAccountDetails = async (req, res) => {
+  // prettier-ignore
+  let { client_id, searchString, page, perPage, start_date, end_date } = req.params
+  // prettier-ignore
+  if (isNaN(page) || isNaN(perPage)) return res.status(200).json({message: 'Pagin Error',data: null,success: false,})
+  page = parseInt(page)
+  perPage = parseInt(perPage)
+  const startingPageForSort = (page - 1) * perPage
+  // prettier-ignore
+  const Account = mongoose.model(`${req.doc._id}-accounts`, require('../models/Account' ) )
+  // prettier-ignore
+  const { findOptions, sortOptions } = await getFindAndSortOptionsAccoToParams(client_id,searchString,start_date,end_date)
+  // prettier-ignore
+  const query =  Account.find(findOptions).sort(sortOptions).skip(startingPageForSort).limit(perPage)
+  const docs = await query.exec()
+  // prettier-ignore
+  if(!docs) return res.status(200).json({message: 'Something went wrong',count: null,data: null,success: false,})
+  // prettier-ignore
+  res.status(200).json({message: 'Accounts after search',count: docs.length,data: docs,success: true,})
+}
 
 module.exports = {
   createAccount,
